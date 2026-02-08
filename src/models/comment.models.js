44c63eac41b1c3170a2218669b1793a5b02b7634
Adityaa2509 <geekysingh2502@gmail.com>
2026-02-08 18:53:01 +0530

    Added like functionality

--- src/models/comment.models.js
+++ src/models/comment.models.js
@@ -3,15 +3,27 @@ import mongoose from "mongoose";
 const commentSchema = new mongoose.Schema({
     content:{
         type: String,
-        required: [true,"Content is required"]
+        required: [true,"Content is required for Comment."]
     },
     video:{
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Video"
+        ref: "Video",
+        required: [true,"Video is required for Comment."]
     },
     owner:{
         type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
+        ref: "User",
+        required: [true,"Owner is required for Comment."]
+    },
+    likes:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ],
+    numberOfLikes:{
+        type:Number,
+        default:0
     }
 },{timestamps: true});
 
