3f89beb5a699da927255d875cbb39b27c60d8e07
Adityaa2509 <geekysingh2502@gmail.com>
2026-02-10 22:09:20 +0530

    Added playlist model

---
+++ src/models/playlist.models.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const playlistSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"Playlist name is required"]
+    },
+    description:{
+        type:String,
+        required:[true,"Description name is required"]
+    },
+    videos:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Video"
+        }
+    ],
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }
+
+});
+
+export const Playlist = mongoose.model("Playlist",playlistSchema);
