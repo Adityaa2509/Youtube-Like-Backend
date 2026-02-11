dafa330bba259e47e122866c299cddbc691c339a
Adityaa2509 <geekysingh2502@gmail.com>
2026-02-11 22:45:49 +0530

    Added controller defination for playlist

---
+++ src/controllers/playlist.controllers.js
@@ -0,0 +1,31 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const createPlaylist = asyncHandler(async(req,resp)=>{
+
+})
+
+const renamePlaylist = asyncHandler(async(req,resp)=>{
+
+})
+
+const addVideoInPlaylist = asyncHandler(async(req,resp)=>{
+
+})
+
+const removeVideoInPlaylist = asyncHandler(async(req,resp)=>{
+
+})
+
+const getUserPlaylists = asyncHandler(async(req,resp)=>{
+
+})
+
+const getPlaylistById = asyncHandler(async(req,resp)=>{
+
+})
+
+const deletePlaylist = asyncHandler(async(req,resp)=>{
+
+})
+
+export {createPlaylist,renamePlaylist,addVideoInPlaylist,removeVideoInPlaylist,getPlaylistById,getUserPlaylists,deletePlaylist}
