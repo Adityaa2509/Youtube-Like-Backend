fa8ece501016fe1f69c4c48fe3b32b8b252a98ba
Adityaa2509 <geekysingh2502@gmail.com>
2026-02-12 22:10:31 +0530

    Added get functionality

--- src/controllers/playlist.controllers.js
+++ src/controllers/playlist.controllers.js
@@ -3,6 +3,8 @@ import { Video } from "../models/video.models.js";
 import { Playlist } from "../models/playlist.models.js"
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { apiError } from "../utils/apiError.js";
+import { apiResponse } from "../utils/apiResponse.js";
+import mongoose from "mongoose";
 
 const createPlaylist = asyncHandler(async(req,resp)=>{
     const user = req?.user;
@@ -19,11 +21,65 @@ const createPlaylist = asyncHandler(async(req,resp)=>{
         throw new apiError(402,"Description is required");
     }
 
-    const playlist = await Playlist.findOne({});
+    const playlist = await Playlist.findOne({
+        $and:[{
+            owner:user._id,
+            name
+        }]
+    });
+    if(playlist){
+        throw new apiError(403,"You already have playlist with this name");
+    }
+
+    const createdPlaylist = await Playlist.create({
+        name,
+        description,
+        owner:user._id
+    })
+
+    if(!createdPlaylist){
+        throw new apiError(500,"Something went wrong while creating Plylist");
+    }
+
+    return resp.status(200).json(
+        new apiResponse(200,createdPlaylist,"Playlist created successfully")
+    );
 })
 
 const renamePlaylist = asyncHandler(async(req,resp)=>{
+    
+    const user = req?.user;
+    if(!user){
+        throw new apiError(400,"Unauthorized acces sinside controller");
+    }
 
+    const playlistId = req.params?.playlistId;
+    if(!playlistId){
+        throw new apiError(402,"PlaylistId is required");
+    }
+    if(!mongoose.isValidObjectId(playlistId)){
+        throw new apiError(402,"PlaylistId is not valid");
+    }
+
+    const {name,description} = req.body;
+    if(!name || name.trim() === ""){
+        throw new apiError(402,"Name is required");
+    }
+    if(!description || description.trim() === ""){
+        throw new apiError(402,"Description is required");
+    }
+    //name handling logic to be handled
+
+    const playlist = await Playlist.findOne({_id:playlistId});
+    if(!playlist){
+        throw new apiError(404,"Playlist not found");
+    }
+
+    //update playlist
+
+    //return response
+    
+    
 })
 
 const addVideoInPlaylist = asyncHandler(async(req,resp)=>{
@@ -35,11 +91,53 @@ const removeVideoInPlaylist = asyncHandler(async(req,resp)=>{
 })
 
 const getUserPlaylists = asyncHandler(async(req,resp)=>{
+    const user = req?.user;
+    if(!user){
+        throw new apiError(400,"Unauthorized Access inside Controller");
+    }
 
+    const playlists = await Playlist.findOne({owner:user._id});
+
+    if(!playlists){
+        throw new apiError(500,"Something went wrong while fetching Playlist");
+    }
+
+    return resp.status(200).json(
+        new apiResponse(200,{
+            data:playlists,
+            numberOfPlaylist:playlists.length
+        },"Playlists fetched Successfully")
+    )
 })
 
 const getPlaylistById = asyncHandler(async(req,resp)=>{
+    const user = req?.user;
+    if(!user){
+        throw new apiError(400,"Unauthorized Access inside Controller");
+    }
+    const playlistId = req.params?.playlistId;
 
+    if(!playlistId){
+        throw new apiError(402,"Playlist id is required");
+    }
+    if(!mongoose.isValidObjectId(playlistId)){
+        throw new apiError(402,"PlaylistId is invalid");
+    }
+
+
+    const playlist = await Playlist.findOne({_id:playlistId});
+
+    if(!playlist){
+        throw new apiError(404,"Playlist not found");
+    }
+    
+    if((!user.isAdmin) && (playlist.owner.toString() !== user._id.toString())){
+        throw new apiError(400,"You are not valid user to fetch this Playlist");
+    }
+
+    return resp.status(200).json(
+        new apiResponse(200,playlist,"Playlist fetched successfully")
+    )
 })
 
 const deletePlaylist = asyncHandler(async(req,resp)=>{
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -5,11 +5,14 @@ import { addVideoInPlaylist, createPlaylist, deletePlaylist, getPlaylistById, ge
 const router = Router();
 
 router.route("/create").post(verifyJWT,createPlaylist);
-router.route("/rename").patch(verifyJWT,renamePlaylist);
+
+router.route("/rename/:playlistId").patch(verifyJWT,renamePlaylist);
 router.route("/add/:playlistId/:videoId").patch(verifyJWT,addVideoInPlaylist);
 router.route("/remove/:playlistId/:videoId").patch(verifyJWT,removeVideoInPlaylist);
+
 router.route("/get").get(verifyJWT,getUserPlaylists);
 router.route("/get/:playlistId").get(verifyJWT,getPlaylistById);
+
 router.route("/delete/:playlistId").delete(verifyJWT,deletePlaylist);
 
 
