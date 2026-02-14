dae2c6279c62b6c2ca112620ff5269135ea02645
Adityaa2509 <geekysingh2502@gmail.com>
2026-02-14 17:10:42 +0530

    ADDED add and remove video from playlist and debug the minor bug in apiError and apiResposne regarding statusCode and error getting correctly populated

--- src/controllers/playlist.controllers.js
+++ src/controllers/playlist.controllers.js
@@ -83,11 +83,129 @@ const renamePlaylist = asyncHandler(async(req,resp)=>{
 })
 
 const addVideoInPlaylist = asyncHandler(async(req,resp)=>{
+    
+    //user validation intital
+    const user = req?.user;
+    if(!user){
+         throw new apiError(400,"Unauthorized access inside controller");
+    }
+
+    //playlist id validation
+    const playlistId = req.params?.playlistId;
+    if(!playlistId){
+        throw new apiError(402,"Playlist Id is required");
+    }
+    if(!mongoose.isValidObjectId(playlistId)){
+        throw new apiError(402,"Invalid Playlist Id");
+    }
+    
+    //video id validation
+    const videoId = req.params?.videoId;
+    if(!videoId){
+        throw new apiError(402,"Video Id is required");
+    }
+    if(!mongoose.isValidObjectId(videoId)){
+        throw new apiError(402,"Invalid Video Id");
+    }
+
+    //fetch playlist
+    const playlist = await Playlist.findOne({_id:playlistId});
+    if(!playlist){
+        throw new apiError(404,"Playlist Not Found");
+    }
+   
+    //fetch video
+    const video = await Video.findOne({_id:videoId});
+    if(!video){
+        throw new apiError(404,"Video Not Found");
+    }
+   
+    //playlist owner and req user validation
+    if(playlist.owner.toString() !== user._id.toString()){
+        throw new apiError(400,"User do not have permission to add video in this playlist");
+    }
+   
+    //check if already added --> if already added leave
+    console.log()
+    const videoFound = playlist.videos.filter((video)=>video.toString() === videoId.toString());
+    if(videoFound.length>0){
+        throw new apiError(400,"Video already added in playlist");
+    }
+
+    //if not already added add it 
+    playlist.videos.push(videoId);
+    const updatedPlaylist =  await playlist.save({new:true});
+    if(!updatedPlaylist){
+        throw new apiError(500,"Something went wrong while adding video in playlist");
+    }
+    
+    //return resp
+    return resp.status(200).json(
+        new apiResponse(200,updatedPlaylist,"Video added successfully in playlist")
+    );
 
 })
 
 const removeVideoInPlaylist = asyncHandler(async(req,resp)=>{
+    //user validation intital
+    const user = req?.user;
+    if(!user){
+         throw new apiError(401,"Unauthorized access inside controller");
+    }
 
+    //playlist id validation
+    const playlistId = req.params?.playlistId;
+    if(!playlistId){
+        throw new apiError(400,"Playlist Id is required");
+    }
+    if(!mongoose.isValidObjectId(playlistId)){
+        throw new apiError(400,"Invalid Playlist Id");
+    }
+    
+    //video id validation
+    const videoId = req.params?.videoId;
+    if(!videoId){
+        throw new apiError(400,"Video Id is required");
+    }
+    if(!mongoose.isValidObjectId(videoId)){
+        throw new apiError(400,"Invalid Video Id");
+    }
+
+    //fetch playlist
+    const playlist = await Playlist.findOne({_id:playlistId});
+    if(!playlist){
+        throw new apiError(404,"Playlist Not Found");
+    }
+   
+    //fetch video
+    const video = await Video.findOne({_id:videoId});
+    if(!video){
+        throw new apiError(404,"Video Not Found");
+    }
+   
+    //playlist owner and req user validation
+    if(playlist.owner.toString() !== user._id.toString()){
+        throw new apiError(404,"User do not have permission to add video in this playlist");
+    }
+   
+    //check if video exists -> is yes then delete if not leave
+    const videoFound = playlist.videos.filter((video)=>video.toString() === videoId.toString());
+    if(videoFound.length == 0){
+        throw new apiError(404,"Video already removed from Playlist");
+    }
+
+    playlist.videos = playlist.videos.
+                      filter((video)=>video.toString() !== videoId.toString());
+    
+    const updatedPlaylist = await playlist.save({new:true});
+    if(!updatedPlaylist){
+        throw new apiError(500,"Something went wrong while removing video from playlist");
+    }
+
+    //return resp
+    return resp.status(200).json(
+        new apiResponse(200,updatedPlaylist,"Video successfully removed from Playlist")
+    );
 })
 
 const getUserPlaylists = asyncHandler(async(req,resp)=>{
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,7 +2,7 @@ const asyncHandler = (requestHandler)=> async (req,resp,next)=>{
     try{
         await requestHandler(req,resp,next)
     }catch(err){
-        resp.status(err.code || 500).json({
+        resp.status(err.statusCode || 500).json({
             success:false,
             message: err.message || " Internal Server Error"
         })
